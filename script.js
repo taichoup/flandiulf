9a14522eb290226565bca9aba67b5a3f0df1c017
taichoup <taichoup@gmx.net>
2015-05-21 22:12:36 +0200

    squelette de form

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sendForm(form) {
-    
+    var sign = document.getElementById("select_sign").value;
+    var category = document.getElementById("select_cat").value;
+    // document.getElementById("select_sign").submit();
+    alert("Sign is " + sign + " and category is " + category);
+    var request = {
+        s: sign,
+        c: category
+    };
+    console.log(request);
+    return request;
 }
