35dd1b40ceab1d4975e2e7a571c504520836b089
taichoup <taichoup@gmx.net>
2015-05-23 18:22:24 +0200

    now the button gets the data and tries to display it

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-var sources = [
-    {
-        name:"journaldesfemmes",   // same as 20 minutes.fr ?
-        url:"http://sante.journaldesfemmes.com/psychologie/horoscope/zodiaque/%s-jour/".replace('%s', "belier"),
-        amour:".//div[@class='layout_main']/table/tbody/tr[6]/",
-        argent:".//div[@class='layout_main']/table/tbody/tr[9]/",
-        sante:".//div[@class='layout_main']/table/tbody/tr[12]/",
-        travail:".//div[@class='layout_main']/table/tbody/tr[15]/"
-    }, {
-        name:"horoscope.fr",
-        url:"http://www.horoscope.fr/horoscopes/horoscope_%s.html".replace('%s', "belier"),
-        amour:".//div[@id='horo-jour-amour']/div/p[2]",
-        argent:".//div[@id='horo-jour-travail']/div[2]/div[2]/p",
-        sante:".//div[@id='horo-jour-bienetre']/div[1]/div[2]/p",
-        travail:".//div[@id='horo-jour-travail']/div[1]/div[2]/p"
-    }, {
-        name:"le parisien",
-        url:"http://astrologie.leparisien.fr/astrologie/zodiaque/%s.html".replace('%s', "belier"),
-        amour:".//article[@class='np-article-predictionvipsign']/section/div/article/p[1]",
-        argent:"",
-        sante:".//article[@class='np-article-predictionvipsign']/section/div/article/p[3]",
-        travail:".//article[@class='np-article-predictionvipsign']/section/div/article/p[2]"
-    }, {
-        name:"elle.fr",
-        url:"http://www.elle.fr/Astro/Horoscope/Quotidien/%s".replace('%s', "belier"),
-        amour:".//article[@id='text']/p[1]",
-        argent:".//article[@id='text']/p[2]",
-        sante:".//article[@id='text']/p[3]",
-        travail:".//article[@id='text']/p[4]"
-    }
-]
 
 function sendForm(form) {
     var sign = document.getElementById("select_sign").value;
@@ -45,22 +14,27 @@ function sendForm(form) {
 
 request = {s:"belier", c:"sante"} // this will later be set differently
 
-var out = ""; // container for fetched values
 
 
-function crawl() {
-    for (i=0; i<sources.length; i++) {
-        var url = sources[i].url;
-        console.log("Fetching: " + sources[i].name)
-
-        $.ajax({
-            url: url,
-            async: true // synchronous is deprecated
-        }).done(function(data) {
-            var dataRetrieved = "bla bla bla bla";
-            console.log("Retrieved " + dataRetrieved);
-            out += dataRetrieved + "\n";
-        });
-    }
-    console.log("-----------------\nParsing done, output:\n"+out);
-}
+$(document).ready(function() {
+    $("button").click(function() {
+        var jqxhr = $.getJSON("astrodump.json", function(responseTxt, statusTxt, xhr) {
+            //blop
+        })
+            .done(function(data) {
+                alert("External content loaded successfully!");
+                var items = [];
+                $.each(data, function(key, val) {
+                    items.push( "<li id='" + key + "'>" + val + "</li>" );
+                });
+                console.log(items);
+                $( "<ul/>", {
+                    "class": "my-new-list",
+                    html: items.join( "" )
+                }).appendTo( "body" );
+            })
+            .fail(function() {
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+            });
+    });
+});
