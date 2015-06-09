f7268f980b91c923de9c1d77e9d6e8bdf67a8a1b
taichoup <taichoup@gmx.net>
2015-06-09 22:43:28 +0200

    yay classes!

--- astrocrawl.py
+++ astrocrawl.py
@@ -6,96 +6,50 @@ import lxml.html as HTML
 import codecs
 import os
 import json
-# from collections import defaultdict
-
-# class WebSource:
-#     "Common base class for all sites with scrapable horoscope."
-#     def __init__(self, name, charset, url, amour_sel, argent_sel, sante_sel, travail_sel):
-#         self.name = name
-#         self.charset = charset
-#         self.url = url
-#         self.argent_sel = argent_sel
-#         self.sante_sel = sante_sel
-#         self.travail_sel = travail_sel
-#         self.amour_sel = amour_sel
-
-# source1 = WebSource("journaldesfemmes", "iso-8859-1", "http://sante.journaldesfemmes.com/psychologie/horoscope/zodiaque/%s-jour/", ".//div[@class='layout_main']/table/tbody/tr[6]/", ".//div[@class='layout_main']/table/tbody/tr[9]/", ".//div[@class='layout_main']/table/tbody/tr[12]/", ".//div[@class='layout_main']/table/tbody/tr[15]/")
-
-# print source1.url
-
-sources = [{
-    "name":"journaldesfemmes",   # same as 20 minutes.fr ?
-    "charset": "iso-8859-1",
-    "url":"http://sante.journaldesfemmes.com/psychologie/horoscope/zodiaque/%s-jour/",
-    "amour":".//div[@class='layout_main']/table/tbody/tr[6]/",
-    "argent":".//div[@class='layout_main']/table/tbody/tr[9]/",
-    "sante":".//div[@class='layout_main']/table/tbody/tr[12]/",
-    "travail":".//div[@class='layout_main']/table/tbody/tr[15]/"
-    }, {
-    "name":"horoscope_fr",
-    "charset":"utf-8",
-    "url":"http://www.horoscope.fr/horoscopes/horoscope_%s.html",
-    "amour":".//div[@id='horo-jour-amour']/div/p[2]",
-    "argent":".//div[@id='horo-jour-travail']/div[2]/div[2]/p",
-    "sante":".//div[@id='horo-jour-bienetre']/div[1]/div[2]/p",
-    "travail":".//div[@id='horo-jour-travail']/div[1]/div[2]/p"
-    }, {
-    "name":"le_parisien",
-    "charset":"utf-8",
-    "url":"http://astrologie.leparisien.fr/astrologie/zodiaque/%s.html",
-    "amour":".//article[@class='np-article-predictionvipsign']/section/div/article/p[1]",
-    "argent":"",
-    "sante":".//article[@class='np-article-predictionvipsign']/section/div/article/p[3]",
-    "travail":".//article[@class='np-article-predictionvipsign']/section/div/article/p[2]"
-    }, {
-    "name":"elle_fr",
-    "charset":"utf-8",
-    "url":"http://www.elle.fr/Astro/Horoscope/Quotidien/%s",
-    "amour":".//article[@id='text']/p[1]",
-    "argent":".//article[@id='text']/p[2]",
-    "sante":".//article[@id='text']/p[3]",
-    "travail":".//article[@id='text']/p[4]"
-    }, {
-    "name": "astrowi_com",
-    "charset":"utf-8",
-    "url":"http://www.astrowi.com/horoscope-astrologie-jour-%s",
-    "amour":".//div[@id='zParaArticle']/p[1]",
-    "argent":".//div[@id='zParaArticle']/p[2]",
-    "sante":".//div[@id='zParaArticle']/p[3]",
-    "travail":".//div[@id='zParaArticle']/p[4]"
-    }
-]
 
 usersigns = ["belier", "gemeaux", "balance", "scorpion", "sagittaire", "taureau", "vierge", "capricorne", "verseau", "lion", "cancer", "poissons"]
 
 categories = ["amour", "sante", "travail", "argent"]
 
-# with open("astrodump.json", "w") as outfile:
-#     response = {site["name"]:{sign:{cat:{} for cat in categories} for sign in usersigns} for site in sources}
-#     print response
-#     for website in sources:
-#         for sign in usersigns:
-#             for category in categories:
-#                 try:
-#                     print u"Recherche d'horoscope sur %s pour %s/%s" % (website["name"], sign, category)
-#                     p = HTML.fromstring(urllib.urlopen(website["url"] % sign).read())
-#                     desc = p.find(website[category]).text.strip()
-#                     sitename = website["name"]
-#                     response[sitename][sign][category] = desc
-#                 except SyntaxError as e:
-#                     continue
-#     json.dump(response, outfile, encoding="utf-8", sort_keys=True, indent=4, separators=(',', ': '))
+class WebSource:
+    "Common base class for all sites with scrapable horoscope."
+    def __init__(self, lang, name, url, amour_sel, argent_sel, sante_sel, travail_sel):
+        self.lang = lang
+        self.name = name
+        # self.charset = charset
+        self.url = url
+        self.amour_sel = amour_sel
+        self.argent_sel = argent_sel
+        self.sante_sel = sante_sel
+        self.travail_sel = travail_sel
+
+    def build_url(self, sign):
+        return self.url % sign
+
+
+source1 = WebSource("fr", "Le Journal des Femmes", "http://sante.journaldesfemmes.com/psychologie/horoscope/zodiaque/%s-jour/", ".//div[@class='layout_main']/table/tbody/tr[6]/", ".//div[@class='layout_main']/table/tbody/tr[9]/", ".//div[@class='layout_main']/table/tbody/tr[12]/", ".//div[@class='layout_main']/table/tbody/tr[15]/")
+source2 = WebSource("fr", "horoscope.fr", "http://www.horoscope.fr/horoscopes/horoscope_%s.html", ".//div[@id='horo-jour-amour']/div/p[2]", ".//div[@id='horo-jour-travail']/div[2]/div[2]/p", ".//div[@id='horo-jour-bienetre']/div[1]/div[2]/p", ".//div[@id='horo-jour-travail']/div[1]/div[2]/p")
+# source3 = WebSource("fr", "Le Parisien", "http://astrologie.leparisien.fr/astrologie/zodiaque/%s.html", ".//article[@class='np-article-predictionvipsign']/section/div/article/p[1]", "", ".//article[@class='np-article-predictionvipsign']/section/div/article/p[3]", ".//article[@class='np-article-predictionvipsign']/section/div/article/p[2]")
+source4 = WebSource("fr", "Elle.fr", "http://www.elle.fr/Astro/Horoscope/Quotidien/%s", ".//article[@id='text']/p[1]", ".//article[@id='text']/p[2]", ".//article[@id='text']/p[3]", ".//div[@id='zParaArticle']/p[4]")
+# source5 = WebSource("fr", "Astrowi.com", "http://www.astrowi.com/horoscope-astrologie-jour-%s", ".//div[@id='zParaArticle']/p[1]", ".//div[@id='zParaArticle']/p[2]", ".//div[@id='zParaArticle']/p[3]", ".//div[@id='zParaArticle']/p[4]")
+
+sources = [source1, source2, source3, source4, source5]
+
+sources2 = [i.__dict__ for i in sources]
+
+
 
 with open("astrodump2.json", "w") as outfile:
     response = []
-    for website in sources:
+    for source in sources:
         for sign in usersigns:
-            for category in categories:
+            for cat in categories:
                 try:
-                    print u"Recherche d'horoscope sur %s pour %s/%s" % (website["name"], sign, category)
-                    p = HTML.fromstring(urllib.urlopen(website["url"] % sign).read())
-                    desc = p.find(website[category]).text.strip()
-                    response.append({"sign":sign, "source":website["name"], "category":category, "content":desc})
-                except SyntaxError as e:
+                    print u"Recherce d'horoscope sur %s pour %s/%s" % (source.name, sign, cat)
+                    p = HTML.fromstring(urllib.urlopen(source.build_url(sign)).read())
+                    selector = source.__dict__["%s_sel" % cat]
+                    desc = p.find(selector).text.strip()
+                    response.append({"sign":sign, "source":source.name, "category":cat, "content":desc})
+                except Exception as e:
                     continue
     json.dump(response, outfile, encoding="utf-8", sort_keys=True, indent=4, separators=(',', ': '))
